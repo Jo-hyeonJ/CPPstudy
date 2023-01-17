0c446e3dc01cb146e524fac0f5267338bfe6f9f7
조준짱 조준짱 <chohs42@gmail.com>
2023-01-17 19:47:16 +0900

    (2023.01.17) namespace, class

---
+++ testfile/(2023.01.16)범위 기반 반복문+동적할당.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <iostream> // C++ 입출력 헤더
+#include <math.h> // 루트 및 수학적 헤더
+#include <cmath> // 다른 수학 라이브러리 헤더
+
+int main()
+{
+	// 범위 기반 반복문
+	/*
+	// 시작과 끝을 알려주지 않아도 처음부터 끝까지 순회하는 반복문이다.
+	// for([자료형] [변수명] : [리스트이름])
+	int data[5] = { 10,20,30,40,50 };
+
+
+	for (auto x : data) //x에 리스트의 값을 담는 식
+		// for (auto& x : data) 참조형 연산자로 리스트에 접근이 가능하다.
+		// (const auto)리스트 값이 바뀔 경우를 대비해 자료형에 const형을 추가할 수 있다.
+	{
+		// 범위 기반 반복문은 인덱스에 접근이 불가능하다 (전체를 불러와야함)
+		x = 1;
+
+		std::cout << x << std::endl;
+
+	}
+
+	for (int i = 0; i < 5; i++) //리스트 값이 바뀌는지 확인하는 반복문
+	{
+		std::cout << data[i] << std::endl;
+
+	}
+
+	*/
+
+
+	//					+댕글링 포인터 : 이미 해제된 메모리				해제가 됐는데 값을 대입하는 식으로 짤 가능성이 있음
+	// 동적 할당		(여기 값을 넣으면 오류 발생할 가능성이 있음) -> 해제된 메모리를 참조하던 ptr에 nullptr값을 넣는 걸로 예외처리한다. (현재 버전은 해당x)
+	// new(메모리 할당), delete(메모리 해제)
+	// = malloc				=free
+
+	int* ptr = new int;
+
+	*ptr = 100;
+
+	std::cout << "ptr이 가르키는 값 : " << *ptr << "\n";
+	std::cout << "ptr의 값 : " << ptr << std::endl;
+
+	delete ptr;
+
+	return 0;
+}
--- testfile/Application.cpp
+++ testfile/Application.cpp
@@ -2,50 +2,92 @@
 #include <math.h> // 루트 및 수학적 헤더
 #include <cmath> // 다른 수학 라이브러리 헤더
 
+using namespace std;
+
+namespace A
+{
+	int value = 10;
+
+	void information()
+	{
+		std::cout << "A 개발자의 영역" << std::endl;
+
+	}
+} // <- 이 영역에서 벗어나면 메모리에서 사라지는 지역 변수이다.
+
+namespace B
+{
+	int value = 20;
+	void information()
+	{
+		std::cout << "B 개발자의 영역" << std::endl;
+
+	}
+
+}
+
+// using
+/*
+// 범위를 생략해서 사용할 수 있는 기능
+// using을 남발하고 네임 스페이스의 겹치는 이름이 있다면
+// 모호성이 발생해 오류의 원인이 된다.
+// 겹치는 이름이 없다면 using의 복수 사용이 가능하다.
+// 절차 지향성을 띄고 있기에 코드 페이지에 최상단에 올리는 것이 일반적이다.
+
+using namespace A;
+using namespace std;
+*/
+
+
+// 클래스란?
+// 사용자 정의 데이터 유형으로 속성(변수)과 함수가 포함되어 있으며,
+// 클래스를 통해 객체를 생성하여 접근하고 사용할 수 있는 집합체입니다.
+// [관련 용어 : 상속, 접근 지정자(캡슐화), 다형성, 추상화]
+
+class Monster
+{
+	// 접근 지정자란?
+	// 클래스 내부의 포함되어 있는 속성에 접근 범위를 제한하는 지정자이다.
+	// 기본 접근 지정자
+	// 접근 지정자를 설정하지 않으면 기본적으로 private 접근 지정자로 설정된다.
+
+public:
+	int health;
+	float damage;
+
+	void Attack()
+	{
+		cout << "공격합니다." << endl;
+	}
+
+};
+
 int main()
 {
-	// 범위 기반 반복문
+	// 이름 공간
 	/*
-	// 시작과 끝을 알려주지 않아도 처음부터 끝까지 순회하는 반복문이다.
-	// for([자료형] [변수명] : [리스트이름])
-	int data[5] = { 10,20,30,40,50 };
+	// 속성을 구분할 수 있도록 유효 범위를 설정하는 영역입니다.
+	// 협업 중 이름의 중복을 막기 위해 개발자끼리의 영역을 설정하는 기능 
 
-	
-	for (auto x : data) //x에 리스트의 값을 담는 식
-		// for (auto& x : data) 참조형 연산자로 리스트에 접근이 가능하다.
-		// (const auto)리스트 값이 바뀔 경우를 대비해 자료형에 const형을 추가할 수 있다.
-	{
-		// 범위 기반 반복문은 인덱스에 접근이 불가능하다 (전체를 불러와야함)
-		x = 1;
+	std::cout << A::value << std::endl;
+	A::information();
 
-		std::cout << x << std::endl;
-
-	}
-
-	for (int i = 0; i < 5; i++) //리스트 값이 바뀌는지 확인하는 반복문
-	{
-		std::cout << data[i] << std::endl;
-
-	}
-
+	std::cout << B::value << std::endl;
+	B::information();
 	*/
 
+	// 클래스의 인스턴스화
+	// 인스턴스화 : 객체를 스택 메모리에 로드하는 과정 
 
-	//					+댕글링 포인터 : 이미 해제된 메모리				해제가 됐는데 값을 대입하는 식으로 짤 가능성이 있음
-	// 동적 할당		(여기 값을 넣으면 오류 발생할 가능성이 있음) -> 해제된 메모리를 참조하던 ptr에 nullptr값을 넣는 걸로 해결한다.
-	// new(메모리 할당), delete(메모리 해제)
-	// = malloc				=free
+	Monster orc;
+	
 
-	int* ptr = new int;
-
-	*ptr = 100;
-
-	std::cout << "ptr이 가르키는 값 : " << *ptr << "\n";
-	std::cout << "ptr의 값 : " << ptr << std::endl;
-
-	delete ptr;
-
-	*ptr = 100;
+	orc.health = 100;
+	orc.damage = 10;
+	orc.Attack();
+	
+	cout << "오크의 체력은" << orc.health << endl;
+	cout << "오크의 힘은" << orc.damage << endl;
 
 	return 0;
 }
