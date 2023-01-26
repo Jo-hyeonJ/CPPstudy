21c7386d7c570484127113a4f2573dc3074fb417
조준짱 조준짱 <chohs42@gmail.com>
2023-01-26 19:48:07 +0900

    (2023.01.26) 상속

---
+++ testfile/(2023.01.25)얕은 복사 깊은복사.h
@@ -0,0 +1,101 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream> // C++ 입출력 헤더
+#include <math.h> // 루트 및 수학적 헤더
+#include <cmath> // 다른 수학 라이브러리 헤더
+
+using namespace std;
+
+class IPhone
+{
+public:
+    int size;
+    int price;
+    int* version;
+
+
+    IPhone()
+    {
+        version = new int;
+        cout << "기본 생성자 호출" << endl;
+    }
+
+    // 복사 생성자
+    IPhone(const IPhone& copyIPhone) // 읽기 전용으로 생성 가능
+    {
+        // 깊은 복사란?
+        // 객체를 복사할 때, 참조 값이 아닌 인스턴스 자체를 새로 복사하여
+        // 서로 다른 메모리를 생성하는 복사이다.
+
+        size = copyIPhone.size;
+        price = copyIPhone.price;
+
+        version = new int; // 동적 메모리로 받아준다. 각각 다른 메모리를 가지게 함.
+        *version = *copyIPhone.version;
+
+        cout << "복사 생성자 호출" << endl;
+
+        cout << "size : " << size << endl;
+        cout << "price : " << price << endl;
+        cout << "version : " << *version << endl;
+    }
+
+    ~IPhone()
+    {
+        delete version;
+    }
+
+};
+
+int main()
+{
+    // 복사 생성자란?
+    /*
+    // 같은 객체를 복사하여 생성시킬 때 호출되는 생성자입니다.
+    IPhone se1;
+
+    se1.price = 100000;
+    se1.size = 5;
+
+    IPhone se2 = se1;
+    */
+
+    // 얕은 복사
+    /*
+    // 객체를 복사할 때 주솟값을 복사하여 같은 메모리를 가르키는 복사이다.
+    // -> 따로 값을 가지지 못하고 메모리 해제시 오류 발생 가능성 있음
+
+    int* ptr1 = new int;
+    *ptr1 = 10000;
+
+    int* ptr2 = ptr1;
+    *ptr2 = 3000;
+
+    cout << "ptr1의 값 : " << *ptr1 << endl;
+    cout << "ptr2의 값 : " << *ptr2 << endl;
+
+    delete ptr1;
+    // delete ptr2; 이미 해제된 메모리를 해제하려 시도한 것 (오류 발생)
+    */
+
+    IPhone se1;
+
+    *se1.version = 10;
+    se1.price = 100000;
+    se1.size = 5;
+
+    IPhone se2 = se1;
+    *se2.version = 20;
+
+    cout << "*se1.version의 값 : " << *se1.version << endl;
+    cout << "*se2.version의 값 : " << *se2.version << endl;
+
+    // 깊은 복사란?
+    // 객체를 복사할 때, 참조 값이 아닌 인스턴스 자체를 새로 복사하여
+    // 서로 다른 메모리를 생성하는 복사이다.
+
+
+
+    return 0;
+}
--- testfile/Application.cpp
+++ testfile/Application.cpp
@@ -6,93 +6,108 @@
 
 using namespace std;
 
-class IPhone
+// #pragma pack(1)
+// 바이트 패딩을 하지 않고 1바이트로 계산하는 전처리기
+// #pragma region
+
+
+class Unit
 {
-public:
-    int size;
-    int price;
-    int* version;
-
-
-    IPhone()
-    {
-        version = new int;
-        cout << "기본 생성자 호출" << endl;
-    }
-
-    // 복사 생성자
-    IPhone(const IPhone& copyIPhone) // 읽기 전용으로 생성 가능
-    {
-        // 깊은 복사란?
-        // 객체를 복사할 때, 참조 값이 아닌 인스턴스 자체를 새로 복사하여
-        // 서로 다른 메모리를 생성하는 복사이다.
-
-        size = copyIPhone.size;
-        price = copyIPhone.price;
-
-        version = new int; // 동적 메모리로 받아준다. 각각 다른 메모리를 가지게 함.
-        *version = *copyIPhone.version;
-
-        cout << "복사 생성자 호출" << endl;
-
-        cout << "size : " << size << endl;
-        cout << "price : " << price << endl;
-        cout << "version : " << *version << endl;
-    }
+public : // 바이트 패딩이 적용 되어 실사용량은 11바이트지만 16바이트를 차지한다.
+    char grade;
+    int health;
+    short mp;
+    int attack;
     
-    ~IPhone()
+    Unit()
     {
-        delete version;
+        cout << "Unit 객체 생성" << endl;
     }
-    
+
+    void Skill(string skillName)
+    {
+        cout << skillName << endl;
+    }
+
+    ~Unit()
+    {
+        cout << "Unit 객체 파괴" << endl;
+    }
+
 };
+
+// protected 상속
+// 상속 받은 속성 중 public 접근 지정자를 모두 protected로 바꾼다.
+//               ↓ 접근 지정자
+class Ghost : protected Unit
+{
+private :
+    int defense = 3;
+
+public :
+    Ghost()
+    {
+        health = 100;
+        attack = 25;
+
+        //Skill("Lock Down");
+        cout << "Ghost 객체 생성" << endl;
+    }
+
+    ~Ghost()
+    {
+        cout << "Ghost 객체 파괴" << endl;
+    }
+};
+
+
+// private 상속
+// 상속 받은 클래스가 private보다 접근 범위가 넓은 접근 지정자를 모두
+// private로 설정하는 상속이다.                    (public, protected)
+
+//           ↓접근 지정자 [private]로 지정시 상속 받은 속성을 모두 private로 바꾼다.
+class Duran : private Ghost
+{
+public :
+    Duran()
+    {
+        health = 300;
+        attack = 30;
+        // defense = 30; private로 설정 되었기 때문에 수정하지못한다.
+        // 하지만 상속 자체는 받는다.
+
+        //Skill("Clocking");
+
+        cout << "Duran 객체 생성" << endl;
+    }
+    ~Duran()
+    {
+        cout << "Duran 객체 파괴" << endl;
+    }
+
+};
+
 
 int main()
 {
-    // 복사 생성자란?
+    // 상속이란?
     /*
-    // 같은 객체를 복사하여 생성시킬 때 호출되는 생성자입니다.
-    IPhone se1;
+    // 상위 클래스의 속성을 하위 클래스가 사용 할 수 있도록 설정해주는 기능이다.
+    
+    Unit unit;
+    //Ghost ghost;
+    
+    
+    cout << "unit의 메모리 크기 : " << sizeof(unit) << endl;
+    //cout << "ghost의 메모리 크기 : " << sizeof(ghost) << endl;
+    
+    Duran duran;
+    cout << "duran의 메모리 크기 : " << sizeof(duran) << endl;
 
-    se1.price = 100000;
-    se1.size = 5;
-
-    IPhone se2 = se1;
+    // 생성자는 상속 받은 클래스를 전부 상위부터 순서대로 불러오고
+    // 소멸자는 불러온 순서의 역순으로 작용한다.
     */
 
-    // 얕은 복사
-    /*
-    // 객체를 복사할 때 주솟값을 복사하여 같은 메모리를 가르키는 복사이다.
-    // -> 따로 값을 가지지 못하고 메모리 해제시 오류 발생 가능성 있음
-
-    int* ptr1 = new int;
-    *ptr1 = 10000;
-
-    int* ptr2 = ptr1;
-    *ptr2 = 3000;
-
-    cout << "ptr1의 값 : " << *ptr1 << endl;
-    cout << "ptr2의 값 : " << *ptr2 << endl;
-
-    delete ptr1;
-    // delete ptr2; 이미 해제된 메모리를 해제하려 시도한 것 (오류 발생)
-    */
-
-    IPhone se1;
-
-    *se1.version = 10;
-    se1.price = 100000;
-    se1.size = 5;
-
-    IPhone se2 = se1;
-    *se2.version = 20;
-
-    cout << "*se1.version의 값 : " << *se1.version << endl;
-    cout << "*se2.version의 값 : " << *se2.version << endl;
-
-    // 깊은 복사란?
-    // 객체를 복사할 때, 참조 값이 아닌 인스턴스 자체를 새로 복사하여
-    // 서로 다른 메모리를 생성하는 복사이다.
 
 
 
